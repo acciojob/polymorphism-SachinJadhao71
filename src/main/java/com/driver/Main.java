09092682b5c37f8a44b2a32df3a707ccab40f311
sachin jadhao <jadhaosachin7171@gmail.com>
2023-06-09 12:54:23 +0530

    polymorphism is done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+
+        public int product(int x, int y) {
+            return 0;
+        }
+
+        public int product(int x, int y, int z) {
+            return 0;
+        }
+
+        public double product(double x, double y) {
+            return 1.0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        p.product(5,5);
+        p.product(5,5,5);
+        p.product(1.0,1.0);
+    }
 
 }
