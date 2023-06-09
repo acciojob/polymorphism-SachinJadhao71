2c65b4f87e716f6856565028c921893a475bb5bf
sachin jadhao <jadhaosachin7171@gmail.com>
2023-06-09 13:06:10 +0530

    dd

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,28 +1,33 @@
 package com.driver;
 
 public class Main {
-    static class Product{
 
-        public int product(int x, int y) {
-            return 0;
-        }
-
-        public int product(int x, int y, int z) {
-            return 0;
-        }
-
-        public double product(double x, double y) {
-            return 1.0;
-        }
-
-    }
 
     public static void main(String[] args) {
         Product p = new Product();
 
         p.product(5,5);
         p.product(5,5,5);
-        double product = p.product(1.0,1.0);
+        p.product(1.0,1.0);
+    }
+
+}
+class Product{
+
+    Product(){
+
+    }
+
+    public int product(int x, int y) {
+        return x;
+    }
+
+    public int product(int x, int y, int z) {
+        return x;
+    }
+
+    public double product(double x, double y) {
+        return x;
     }
 
 }
